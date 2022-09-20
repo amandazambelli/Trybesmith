049930111c507c29f4a69d2206a3f6bca6665076
Amanda Zambelli <amandazambelli@ymail.com>
2022-09-20 16:33:12 -0300

    adicionando rota de listar pedidos

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -3,7 +3,14 @@ import connection from './connection';
 
 const orderModel = {
   async getAll(): Promise<IOrder[]> {
-    const [ordersData] = await connection.execute('SELECT * FROM Trybesmith.Orders');
+    const [ordersData] = await connection.execute(
+      `SELECT ord.id, ord.userId, JSON_ARRAYAGG(prod.id) AS productsIds
+      FROM Trybesmith.Orders AS ord
+      JOIN Trybesmith.Products AS prod
+      ON prod.orderId = ord.id
+      GROUP BY ord.id
+      ORDER BY ord.userId`,
+    );
 
     return ordersData as IOrder[];
   },
